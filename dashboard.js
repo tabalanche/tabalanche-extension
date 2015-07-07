9b476269125f27d04ff0d148046b15dc0b8250e4
Stuart P. Bentley <stuart@testtrack4.com>
2015-07-07 03:38:59 -0700

    (Sloppily) fix render issues

--- dashboard.js
+++ dashboard.js
@@ -12,13 +12,17 @@ tabalanche.getAllTabGroups(function(tabGroups) {
     for (var j = 0; j < tabGroup.tabs.length; j++) {
       var tab = tabGroup.tabs[j];
       var tabIcon = document.createElement('img');
-      tabIcon.src = tab.icon;
+      var tabDomain = tab.url &&
+        tab.url.replace(/^https?:\/?\/?([^\/]*)\/.*/, '$1');
+      tabIcon.src = tab.icon ||
+        'https://www.google.com/s2/favicons?domain=' + tabDomain;
       var tabLink = document.createElement('a');
       var tabLi = document.createElement('li');
       tabLink.appendChild(tabIcon);
       tabLink.appendChild(document.createTextNode(' ' + tab.title));
       tabLink.href = tab.url;
       tabLi.appendChild(tabLink);
+      tabList.appendChild(tabLi);
     }
     groupContainer.appendChild(groupName);
     groupContainer.appendChild(tabList);
