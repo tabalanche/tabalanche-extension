7391870a0e621199d8d7ff66bcdc2b12ae900e35
Stuart P. Bentley <stuart@testtrack4.com>
2015-07-09 01:26:26 +0000

    Refactor dashboard population a little

--- dashboard.js
+++ dashboard.js
@@ -2,31 +2,49 @@
 
 var tabGroupContainer = document.getElementById('tab-groups');
 
+var tabGroupElems = new Map();
+
+function populateTabList(listElement, tabs) {
+  for (var j = 0; j < tabs.length; j++) {
+    var tab = tabs[j];
+    var tabIcon = document.createElement('img');
+    tabIcon.className = 'tabicon';
+    var tabDomain = tab.url &&
+      tab.url.replace(/^https?:\/?\/?([^\/]*)\/.*/, '$1');
+    tabIcon.src = tab.icon ||
+      'https://www.google.com/s2/favicons?domain=' + tabDomain;
+    var tabLink = document.createElement('a');
+    var tabLi = document.createElement('li');
+    tabLink.appendChild(tabIcon);
+    tabLink.appendChild(document.createTextNode(' ' + tab.title));
+    tabLink.href = tab.url;
+    tabLi.appendChild(tabLink);
+    listElement.appendChild(tabLi);
+  }
+}
+
+function createTabGroupDiv(tabGroup) {
+  var container = document.createElement('div');
+
+  var name = document.createElement('h2');
+  name.textContent = tabGroup.name;
+
+  var list = document.createElement('ul');
+  populateTabList(list, tabGroup.tabs);
+
+  container.appendChild(name);
+  container.appendChild(list);
+
+  tabGroupContainer.appendChild(container);
+  tabGroupElems.set(tabGroup._id, {
+    container: container,
+    list: list,
+    name: name
+  });
+}
+
 tabalanche.getAllTabGroups(function(tabGroups) {
   for (var i = 0; i < tabGroups.length; i++) {
-    var tabGroup = tabGroups[i];
-    var groupContainer = document.createElement('div');
-    var groupName = document.createElement('h2');
-    groupName.textContent = tabGroup.name;
-    var tabList = document.createElement('ul');
-    for (var j = 0; j < tabGroup.tabs.length; j++) {
-      var tab = tabGroup.tabs[j];
-      var tabIcon = document.createElement('img');
-      tabIcon.className = 'tabicon';
-      var tabDomain = tab.url &&
-        tab.url.replace(/^https?:\/?\/?([^\/]*)\/.*/, '$1');
-      tabIcon.src = tab.icon ||
-        'https://www.google.com/s2/favicons?domain=' + tabDomain;
-      var tabLink = document.createElement('a');
-      var tabLi = document.createElement('li');
-      tabLink.appendChild(tabIcon);
-      tabLink.appendChild(document.createTextNode(' ' + tab.title));
-      tabLink.href = tab.url;
-      tabLi.appendChild(tabLink);
-      tabList.appendChild(tabLi);
-    }
-    groupContainer.appendChild(groupName);
-    groupContainer.appendChild(tabList);
-    tabGroupContainer.appendChild(groupContainer);
+    createTabGroupDiv(tabGroups[i]);
   }
 });
