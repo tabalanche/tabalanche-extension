1c7fc7d5c996a5121385c0be47481554910b06bf
Stuart P. Bentley <stuart@testtrack4.com>
2015-07-10 02:18:51 +0000

    Introduce input binding for options

--- chrome/options.js
+++ chrome/options.js
@@ -1,13 +1,22 @@
 /* global chrome */
 
+var boundInputs = [
+  {id: 'magicnewtab', opt: 'magicNewTab', def: true},
+  {id: 'saveicons', opt: 'saveIcons', def: true},
+];
+
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var magicNewTab = document.getElementById('magicnewtab').checked;
-  var saveIcons = document.getElementById('saveicons').checked;
-  chrome.storage.sync.set({
-    magicNewTab: magicNewTab,
-    saveIcons: saveIcons
-  }, function() {
+  var opts = {};
+  for (var i = 0; i < boundInputs.length; i++) {
+    var input = document.getElementById(boundInputs[i].id);
+    // right now this is the only input we have
+    if (input.type == 'checkbox') {
+      opts[boundInputs[i].opt] = input.checked;
+    }
+  }
+
+  chrome.storage.sync.set(opts, function() {
     // Update status to let user know options were saved.
   });
 }
@@ -15,15 +24,20 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
+  var opts = {};
+  for (var i = 0; i < boundInputs.length; i++) {
+    opts[boundInputs[i].opt] = boundInputs[i].def;
+  }
   // Use default values.
-  chrome.storage.sync.get({
-    magicNewTab: true,
-    saveIcons: true
-  }, function(items) {
-    document.getElementById('magicnewtab').checked = items.magicNewTab;
-    document.getElementById('saveicons').checked = items.saveIcons;
+  chrome.storage.sync.get(opts, function(items) {
+    for (var i = 0; i < boundInputs.length; i++) {
+      var input = document.getElementById(boundInputs[i].id);
+      if (input.type == 'checkbox') {
+        input.checked = items[boundInputs[i].opt];
+      }
+    }
   });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click', save_options);
