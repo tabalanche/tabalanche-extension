3c8cca17f0783992aae6ef591d8e686c656913d9
Stuart P. Bentley <stuart@testtrack4.com>
2015-07-10 03:48:06 +0000

    More command magic

--- chrome/event-page.js
+++ chrome/event-page.js
@@ -1,8 +1,5 @@
 /* global chrome tabalanche */
 
-chrome.omnibox.setDefaultSuggestion({
-  description:'<dim>command</dim>'});
-
 var actions = [
   ['stash', '(this tab)', tabalanche.stashThisTab],
   ['stash tab', '(this tab)', tabalanche.stashThisTab],
@@ -20,18 +17,59 @@ function matchingActions(prefix) {
   });
 }
 
+function escapeXml(unsafe) {
+  return unsafe.replace(/[<>&'"]/g, function (c) {
+    switch (c) {
+      case '<': return '&lt;';
+      case '>': return '&gt;';
+      case '&': return '&amp;';
+      case '\'': return '&apos;';
+      case '"': return '&quot;';
+    }
+  });
+}
+
+function matchDescription(text, tuple) {
+  return '<match>' + escapeXml(text) + '</match>' +
+    escapeXml(tuple[0].slice(0, text.length)) +
+    ' <dim>' +  escapeXml(tuple[1]) + '</dim>';
+}
+
+function definiteIntent(possibilities) {
+  var action = possibilities.reduce(function (n, m) {
+    return n ? (n==m[2] ? n : true) : m[2];
+  }, null);
+
+  // flip our conflict sentinel value so it makes sense
+  if (action === true) action = false;
+
+  return action;
+}
+
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-  return suggest(matchingActions(text).map(function (tuple) {
+  var actions = matchingActions(text);
+  if (definiteIntent(actions)) {
+    chrome.omnibox.setDefaultSuggestion({
+      description: matchDescription(text, actions[0])});
+    actions = actions.slice(1);
+  } else if (actions.length > 0) {
+    chrome.omnibox.setDefaultSuggestion({
+      description: '<match>' + escapeXml(text) +
+      '</match> <dim>(ambiguous)</dim>'});
+  } else {
+    chrome.omnibox.setDefaultSuggestion({
+      description: escapeXml(text) + ' <dim>(no match)</dim>'});
+  }
+  return suggest(actions.map(function (tuple) {
     return {content: tuple[0],
-      description: tuple[0] + ' <dim>' +  tuple[1] + '</dim>'};
+      description: matchDescription(text, tuple)};
   }));
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text, disposition) {
   // Get unique action, or null if no action, or true if multiple actions
-  var action = matchingActions(text).reduce(function (n, m) {
-    return n ? (n==m[2] ? n : true) : m[2];
-  }, null);
+  var action = definiteIntent(matchingActions(text));
+
   // if there was only one action
   if (action && action !== true) {
     // do it
