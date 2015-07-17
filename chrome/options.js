c7c49159ca3945860bb8895fa697ac332844f7ea
Stuart P. Bentley <stuart@testtrack4.com>
2015-07-17 23:34:57 +0000

    First pass at whitelisting data icons

--- chrome/options.js
+++ chrome/options.js
@@ -1,9 +1,11 @@
-/* global chrome */
+/* global chrome platform */
 
 var boundInputs = [
-  {id: 'savelinkicons', opt: 'saveLinkIcons', def: true},
-  {id: 'savedataicons', opt: 'saveDataIcons', def: false},
+  {id: 'savelinkicons', opt: 'saveLinkIcons'},
+  {id: 'ignorepin', opt: 'ignorePinnedTabs'},
 ];
+
+var whitelistTextarea = document.getElementById('whitelist');
 
 // Saves options to chrome.storage.sync.
 function save_options() {
@@ -15,6 +17,12 @@ function save_options() {
       opts[boundInputs[i].opt] = input.checked;
     }
   }
+  var whitelistValue = whitelistTextarea.value.trim();
+  if (whitelistValue === platform.defaultDataIconWhitelist) {
+    opts.dataIconWhitelist = null;
+  } else {
+    opts.dataIconWhitelist = whitelistValue;
+  }
 
   chrome.storage.sync.set(opts, function() {
     // Update status to let user know options were saved.
@@ -24,20 +32,39 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  var opts = {};
-  for (var i = 0; i < boundInputs.length; i++) {
-    opts[boundInputs[i].opt] = boundInputs[i].def;
-  }
-  // Use default values.
-  chrome.storage.sync.get(opts, function(items) {
+  chrome.storage.sync.get(platform.optionDefaults, function(items) {
     for (var i = 0; i < boundInputs.length; i++) {
       var input = document.getElementById(boundInputs[i].id);
       if (input.type == 'checkbox') {
         input.checked = items[boundInputs[i].opt];
       }
     }
+    if (items.dataIconWhitelist === null) {
+      whitelistTextarea.value = platform.defaultDataIconWhitelist;
+    } else {
+      whitelistTextarea.value = items.dataIconWhitelist;
+    }
   });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
+
+var advancedDiv = document.getElementById('advanced');
+var advancedLink = document.getElementById('show-advanced');
+
+advancedLink.addEventListener('click', function (evt) {
+  if (advancedDiv.hidden) {
+    advancedDiv.hidden = false;
+    advancedLink.textContent = 'Hide advanced options...';
+  } else {
+    advancedDiv.hidden = true;
+    advancedLink.textContent = 'Show advanced options...';
+  }
+});
+
+var revertWhitelistButton = document.getElementById('load-default-whitelist');
+
+revertWhitelistButton.addEventListener('click', function (evt) {
+  whitelistTextarea.value = platform.defaultDataIconWhitelist;
+});
--- platform.js
+++ platform.js
@@ -3,6 +3,19 @@
 var platform = {};
 (function(){
 
+var defaultDataIconWhitelist = ['trello.com'];
+var optionDefaults = {
+  ignorePinnedTabs: true,
+  saveLinkIcons: true,
+  // the default for the whitelist value is 'null' so we know to skip ahead
+  // and load the default list without going through the split/trim step
+  dataIconWhitelist: null
+};
+
+// exposed for the options page
+platform.optionDefaults = optionDefaults;
+platform.defaultDataIconWhitelist = defaultDataIconWhitelist.join('\n');
+
 platform.getCurrentWindowContext = function getCurrentWindowContext() {
   return new Promise(function(resolve, reject) {
     chrome.windows.getCurrent({populate: false}, function (crWindow) {
@@ -14,33 +27,73 @@ platform.getCurrentWindowContext = function getCurrentWindowContext() {
 
 platform.getWindowTabs = {};
 
+function whitelistMatcher(whitelist) {
+  whitelist = whitelist ?
+    // For each line
+    whitelist.split('\n')
+      // remove indentation / whitespace-at-EOL
+      .map(function(s){return s.trim})
+      // remove blank lines and lines with comments
+      .filter(function(s){return s && s[0] != '#'})
+  : defaultDataIconWhitelist;
+
+  // Quick shortcuts
+  if (whitelist.length == 0) {
+    return function(s) {return false;};
+  // if global whitelisting has been enabled
+  } else if (whitelist.indexOf('*') > -1) {
+    return function(s) {return true;};
+  }
+
+  // any number of domain levels before this
+  var arbitraryPrefix = '(?:[a-zA-Z0-9_-]*\\.)*';
+
+  // Convert each item to a regex
+  // TODO: handle paths
+  for (var i = 0; i < whitelist.length; i++) {
+    var prefix = whitelist[i].slice(0,2) == '*.' ? arbitraryPrefix : '';
+    var item = prefix ? whitelist[i].slice(2) : whitelist[i];
+    whitelist[i] = prefix + item.replace(/\./g, '\\.');
+  }
+
+  // it's okay to be this strict because we're comparing to normalized URLs
+  var finalRegexp = new RegExp('^https?://(?:' + whitelist.join('|') + ')/');
+  return function testWhitelistRegex(s) {
+    return finalRegexp.test(s);
+  };
+}
+
 function queryCurrentWindowTabs (params) {
 
   params.currentWindow = true;
 
   return new Promise(function(resolve) {
-    chrome.storage.sync.get({
-      ignorePinnedTabs: true,
-      saveLinkIcons: true,
-      saveDataIcons: false
-    }, function(opts) {
+    chrome.storage.sync.get(optionDefaults, function(opts) {
+      var isDataIconOkay = whitelistMatcher(opts.dataIconWhitelist);
+
+      function filterTabData(tabs) {
+
+        // For every tab in the list
+        for (var i = 0; i < tabs.length; i++) {
+
+          // If the favicon is one of the types we don't save
+          if (/^chrome:/.test(tabs[i].favIconUrl) ||
+            /^data:/.test(tabs[i].favIconUrl) ?
+              !isDataIconOkay(tabs[i].url) : !opts.saveLinkIcons) {
+
+            // Remove the favicon data
+            tabs[i].favIconUrl = '';
+          }
+        }
+        return tabs;
+      }
 
       if (opts.ignorePinnedTabs) {
         params.pinned = false;
       }
 
       return chrome.tabs.query(params, function(tabs) {
-
-        for (var i = 0; i < tabs.length; i++) {
-          if (/^chrome:/.test(tabs[i].favIconUrl) ||
-            /^data:/.test(tabs[i].favIconUrl) ?
-              !opts.saveDataIcons : !opts.saveLinkIcons) {
-
-            tabs[i].favIconUrl = '';
-          }
-        }
-
-        return resolve(tabs);
+        return resolve(filterTabData(tabs));
       });
     });
   });
