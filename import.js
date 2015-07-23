9ecb3b62c6217b55633cd613102d281e6ece34f3
Stuart P. Bentley <stuart@testtrack4.com>
2015-07-23 07:16:31 +0000

    Add a CLICK event listener, kadoy

--- import.js
+++ import.js
@@ -2,9 +2,11 @@
 
 var dumpTextArea = document.getElementById('dump');
 
-document.getElementById('import-button').addEventListener(function(evt) {
+function importDump() {
   var tabGroups = JSON.parse(dumpTextArea.value);
   for (var i = 0; i < tabGroups.length; i++) {
     tabalanche.importTabGroup(tabGroups[i]);
   }
-});
+}
+
+document.getElementById('import-button').addEventListener('click', importDump);
