a7b5ea9f260b7bab70e73c08eeb02659416355dd
Stuart P. Bentley <stuart@testtrack4.com>
2015-07-23 07:20:35 +0000

    Don't export the _rev property
    It's kind of an implementation detail, I don't like exposing it

--- export.js
+++ export.js
@@ -3,5 +3,8 @@
 var dumpTextArea = document.getElementById('dump');
 
 tabalanche.getAllTabGroups().then(function(tabGroups) {
+  tabGroups.forEach(function(group) {
+    delete group._rev;
+  });
   dumpTextArea.value = JSON.stringify(tabGroups, null, 2);
 });
