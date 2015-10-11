3a768da6a79895d14e3158c72a924e6382f4ea40
Stuart P. Bentley <stuart@testtrack4.com>
2015-10-11 08:39:34 +0000

    Monitor import progress

--- scripts/import.js
+++ scripts/import.js
@@ -1,15 +1,34 @@
 /* global tabalanche platform */
 
 var dumpTextArea = document.getElementById('dump');
+var importButton = document.getElementById('import-button');
+var importProgress = document.getElementById('import-progress');
 
 function importDump() {
   var tabGroups = JSON.parse(dumpTextArea.value);
+
+  importProgress.max = tabGroups.length;
+  var imported = 0;
+  importProgress.value = imported;
+  importProgress.textContent = '0 / ' + tabGroups.length;
+
+  importButton.hidden = true;
+  importProgress.hidden = false;
+
   for (var i = 0; i < tabGroups.length; i++) {
-    tabalanche.importTabGroup(tabGroups[i]);
+    tabalanche.importTabGroup(tabGroups[i]).then(function(){
+      importProgress.value = ++imported;
+      importProgress.textContent = imported + ' / ' + tabGroups.length;
+      if (imported == tabGroups.length) {
+        importButton.textContent = 'Import complete';
+        importButton.hidden = false;
+        importProgress.hidden = true;
+      }
+    });
   }
 }
 
-document.getElementById('import-button').addEventListener('click', importDump);
+importButton.addEventListener('click', importDump);
 
 var optslink = document.getElementById('options');
 
