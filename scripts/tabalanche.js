ac40ba73e159cad916e241de27e71b04dbdce033
Stuart P. Bentley <s@stuartpb.com>
2025-11-14 02:07:38 -0800

    Ditch design doc

--- scripts/tabalanche.js
+++ scripts/tabalanche.js
@@ -2,52 +2,9 @@
 
 var tabalanche = {};
 (function(){
-  var dashboardDesignDoc = {
-    _id: '_design/dashboard',
-    version: 2,
-    views: {
-      by_creation: {
-        map: function(doc) {
-          emit([doc.created, doc._id]);
-        }.toString()
-      },
-      total_tabs: {
-        map: function(doc) {
-          emit(doc._id, doc.tabs.length);
-        }.toString(),
-        reduce: '_sum'
-      }
-    }
-  };
-
-  function ensureCurrentDesignDoc(db, designDoc) {
-    function checkAgainstExistingDesignDoc(existing) {
-
-      // If we have a newer design doc than the current one
-      if (designDoc.version > existing.version) {
-
-        // Note the revision we're clobbering and try the put again
-        designDoc._rev = existing._rev;
-        return ensureCurrentDesignDoc(db, designDoc);
-
-      // If the existing design doc appears to be up to date then
-      // return the DB for stuff like getDB
-      } else return db;
-    }
-
-    return db.put(designDoc).then(function() {
-      // return the DB for stuff like getDB
-      return db;
-    }).catch(function (err) {
-      if (err.name == 'conflict') {
-        return db.get(designDoc._id)
-          .then(checkAgainstExistingDesignDoc);
-      } else throw(err);
-    });
-  }
 
   var tabgroups = new PouchDB('tabgroups');
-  var tabgroupsReady = ensureCurrentDesignDoc(tabgroups, dashboardDesignDoc);
+  var tabgroupsReady = tabgroups;
 
   function stashTabs(tabs) {
     return platform.currentWindowContext().then(function(store) {
@@ -62,12 +19,15 @@ var tabalanche = {};
 
       return tabgroupsReady.then(function() {
         if (tabs.length > 0) {
+          var uuid = crypto.randomUUID().toUpperCase();
           var tabGroupDoc = {
+            _id: stashTime.toISOString() + '_' + uuid,
+            uuid,
             created: stashTime.getTime(),
             tabs: tabs.map(stashedTab)
           };
 
-          return tabgroups.post(tabGroupDoc).then(function(response) {
+          return tabgroups.put(tabGroupDoc).then(function(response) {
             platform.closeTabs(tabs);
             var dashboard = platform.extensionURL('dashboard.html');
             open(dashboard + '#' + response.id, '_blank');
@@ -94,6 +54,7 @@ var tabalanche = {};
 
   tabalanche.importTabGroup = function importTabGroup(tabGroup, opts) {
     opts = opts || {};
+    // TODO: migrate schema during import
     return tabgroupsReady.then(function() {
       if (tabGroup._id) {
         return tabgroups.put({
@@ -112,7 +73,7 @@ var tabalanche = {};
 
   tabalanche.getAllTabGroups = function() {
     return tabgroupsReady.then(function () {
-      return tabgroups.query('dashboard/by_creation', {
+      return tabgroups.allDocs({
         include_docs: true,
         descending: true
       }).then(function (response) {
@@ -136,7 +97,7 @@ var tabalanche = {};
     }
 
     return tabgroupsReady.then(function () {
-      return tabgroups.query('dashboard/by_creation', queryOpts)
+      return tabgroups.allDocs(queryOpts)
       .then(function (response) {
         return response.rows.map(function (row) {
           return row.doc;
