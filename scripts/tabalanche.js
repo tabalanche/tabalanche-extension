b5f6c7027fe82e213c4aa96b419acc8ba331e8fe
Stuart P. Bentley <s@stuartpb.com>
2025-11-14 17:31:03 -0800

    Migrate within lock

--- scripts/tabalanche.js
+++ scripts/tabalanche.js
@@ -5,9 +5,20 @@ var tabalanche = {};
 
   var tabgroups = new PouchDB('tabgroups');
 
+  // these numbers aren't gonna be exact due to design docs / new stashes happening mid-migration
+  // but they're good enough to explain that a slowdown on load is going to end someday
+  let totalTabGroups = 'many';
+  let currentGroup = 0;
+
+  function updateProgress() {
+    localStorage.setItem("zeroDesignDocMigration",++currentGroup);
+    localStorage.setItem("slowBanner", `Updating ${
+      currentGroup}/${totalTabGroups} stashes for Tabalanche 1.2.0`)
+  }
+
   function postDesignDocMigration(doc) {
     if (doc._id[0] == '_') return false;
-    
+
     const uuid = doc.uuid || doc._id || crypto.randomUUID().toUpperCase();
     const created = new Date(doc.created).toISOString();
 
@@ -19,16 +30,24 @@ var tabalanche = {};
     // ensure document timestamp is in ISO format
     doc.created = created;
 
+    updateProgress();
+
     return doc;
   }
 
-  alreadyMigrated = localStorage.getItem("postDesignDocMigration");
+  const migrated = navigator.locks.request("zeroDesignDocMigration", async () => {
+    const migrationStatus = localStorage.getItem("zeroDesignDocMigration");
+    if (migrationStatus == 'complete') return tabgroups;
 
-  var tabgroupsReady = alreadyMigrated ? tabgroups.info().then(()=>tabgroups)
-    : tabgroups.migrate(postDesignDocMigration).then(()=>{
-      localStorage.setItem("postDesignDocMigration", "complete");
+    totalTabGroups = (await tabgroups.info()).doc_count;
+    currentGroup = +migrationStatus;
+
+    return tabgroups.migrate(postDesignDocMigration).then(() => {
+      localStorage.setItem("zeroDesignDocMigration", "complete");
+      localStorage.removeItem("slowBanner");
       return tabgroups;
     });
+  });
 
   function stashTabs(tabs) {
     return platform.currentWindowContext().then(function(store) {
@@ -41,25 +60,23 @@ var tabalanche = {};
         };
       }
 
-      return tabgroupsReady.then(function() {
-        if (tabs.length > 0) {
-          var uuid = crypto.randomUUID().toUpperCase();
-          var tabGroupDoc = {
-            _id: stashTime.toISOString() + '_' + uuid,
-            uuid,
-            created: stashTime.toISOString(),
-            tabs: tabs.map(stashedTab)
-          };
+      if (tabs.length > 0) {
+        var uuid = crypto.randomUUID().toUpperCase();
+        var tabGroupDoc = {
+          _id: stashTime.toISOString() + '_' + uuid,
+          uuid,
+          created: stashTime.toISOString(),
+          tabs: tabs.map(stashedTab)
+        };
 
-          return tabgroups.put(tabGroupDoc).then(function(response) {
-            platform.closeTabs(tabs);
-            var dashboard = platform.extensionURL('dashboard.html');
-            open(dashboard + '#' + response.id, '_blank');
-          });
-        } else {
-          throw new Error('No tabs to save');
-        }
-      });
+        return tabgroups.put(tabGroupDoc).then(function(response) {
+          platform.closeTabs(tabs);
+          var dashboard = platform.extensionURL('dashboard.html');
+          open(dashboard + '#' + response.id, '_blank');
+        });
+      } else {
+        throw new Error('No tabs to save');
+      }
     });
   }
 
@@ -78,29 +95,27 @@ var tabalanche = {};
 
   tabalanche.importTabGroup = function importTabGroup(tabGroup, opts) {
     opts = opts || {};
-    return tabgroupsReady.then(function() {
-      if (tabGroup.uuid) {
-        return tabgroups.put({
-          _id: tabGroup._id,
-          uuid: tabGroup.uuid,
-          created: tabGroup.created,
-          tabs: tabGroup.tabs
-        });
-      } else {
-        const uuid = tabGroup._id || crypto.randomUUID().toUpperCase();
-        const created = new Date(tabGroup.created).toISOString();
-        return tabgroups.put({
-          _id: `${created}_${uuid}`,
-          uuid: uuid,
-          created: created,
-          tabs: tabGroup.tabs
-        });
-      }
-    });
+    if (tabGroup.uuid) {
+      return tabgroups.put({
+        _id: tabGroup._id,
+        uuid: tabGroup.uuid,
+        created: tabGroup.created,
+        tabs: tabGroup.tabs
+      });
+    } else {
+      const uuid = tabGroup._id || crypto.randomUUID().toUpperCase();
+      const created = new Date(tabGroup.created).toISOString();
+      return tabgroups.put({
+        _id: `${created}_${uuid}`,
+        uuid: uuid,
+        created: created,
+        tabs: tabGroup.tabs
+      });
+    }
   };
 
   tabalanche.getAllTabGroups = function() {
-    return tabgroupsReady.then(function () {
+    return migrated.then(function () {
       return tabgroups.allDocs({
         include_docs: true,
         descending: true
@@ -112,7 +127,7 @@ var tabalanche = {};
     });
   };
 
-  tabalanche.getSomeTabGroups = function(startKey) {
+  tabalanche.getSomeTabGroups = async function(startKey) {
     var queryOpts = {
       include_docs: true,
       descending: true,
@@ -124,18 +139,18 @@ var tabalanche = {};
       queryOpts.skip = 1;
     }
 
-    return tabgroupsReady.then(function () {
-      return tabgroups.allDocs(queryOpts)
-      .then(function (response) {
-        return response.rows.map(function (row) {
-          return row.doc;
-        });
+    await migrated;
+
+    return tabgroups.allDocs(queryOpts)
+    .then(function (response) {
+      return response.rows.map(function (row) {
+        return row.doc;
       });
     });
   };
 
   tabalanche.getDB = function getDB() {
-    return tabgroupsReady.then();
+    return tabgroups.info().then(() => tabgroups);
   };
 
   tabalanche.destroyAllTabGroups = function destroyAllTabGroups() {
